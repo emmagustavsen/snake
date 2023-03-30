c1d13128e5a0b4271369380a185c90aa95d349d6
jasmineathea <jna009@uib.no>
2023-03-30 20:30:50 +0200

    Added some parts of controller

--- src/main/java/no/uib/inf101/sem2/snake/Main.java
+++ src/main/java/no/uib/inf101/sem2/snake/Main.java
@@ -2,6 +2,7 @@ package no.uib.inf101.sem2.snake;
 
 import javax.swing.JFrame;
 
+import no.uib.inf101.sem2.snake.controller.SnakeController;
 import no.uib.inf101.sem2.snake.model.SnakeModel;
 import no.uib.inf101.sem2.snake.view.SnakeView;
 
@@ -9,6 +10,7 @@ public class Main {
   public static void main(String[] args) {
     SnakeModel model = new SnakeModel();
     SnakeView view = new SnakeView(model);
+    SnakeController controller = new SnakeController(model, view);
 
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/main/java/no/uib/inf101/sem2/snake/controller/SnakeController.java
+++ src/main/java/no/uib/inf101/sem2/snake/controller/SnakeController.java
@@ -1,5 +1,61 @@
 package no.uib.inf101.sem2.snake.controller;
 
-public class SnakeController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
+import no.uib.inf101.sem2.snake.midi.Song;
+import no.uib.inf101.sem2.snake.model.SnakeModel;
+import no.uib.inf101.sem2.snake.view.SnakeView;
+
+/**
+ * Controller for Snake, tracking the keys pressed by the user. Implements KeyListener and ActionListener.
+ * 
+ * @author Jasmine Næss - jasmine.ness@student.uib.no
+ */
+public class SnakeController implements KeyListener, ActionListener {
+
+    SnakeControllable controller;
+    SnakeView viewer;
+    Timer timer;
+    Song song;
     
+    /**
+     * Class constructor.
+     * 
+     * @param controller
+     * @param viewer
+     */
+    public SnakeController(SnakeControllable controller, SnakeView viewer) {
+        this.controller = controller;
+        this.viewer = viewer;
+        viewer.addKeyListener(this);
+        this.song = new Song();
+    }
+
+
+    public SnakeController(SnakeModel model, SnakeView view) {
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            song.run();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
 }
